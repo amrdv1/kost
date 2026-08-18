ef9546a836c6c3da42d25540de07a07bbc5f1b77
alima <alima@DESKTOP-LE5SPSM>
2026-08-18 15:26:46 +0400

    Add tiktok countdown variant

--- server.js
+++ server.js
@@ -603,6 +603,7 @@ app.use(express.static(path.join(__dirname, 'frontend')));
 // Routes
 app.get('/admin', (req, res) => res.sendFile(path.join(__dirname, 'frontend', 'admin', 'index.html')));
 app.get('/countdown', (req, res) => res.sendFile(path.join(__dirname, 'frontend', 'countdown', 'index.html')));
+app.get('/tiktok', (req, res) => res.sendFile(path.join(__dirname, 'frontend', 'tiktok', 'index.html')));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'frontend', 'client', 'index.html')));
 
 // --- GRACEFUL SHUTDOWN ---
